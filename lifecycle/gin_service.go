7987c67dfa5cde24905cf86b013a3db2c668055f
洮羱芝闇 <shun_@outlook.com>
2024-03-14 09:36:44 +0800

    创建项目雏形

---
+++ lifecycle/gin_service.go
@@ -0,0 +1,39 @@
+package lifecycle
+
+import (
+	"com.mutantcat.cloud_step/router"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// 初始化Gin服务
+func InitGin() *gin.Engine {
+	ginServer := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS", "PUT"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Health-Info"}
+	ginServer.Use(cors.New(config))
+	return ginServer
+}
+
+// 启动Gin服务
+func StartGin(ginServer *gin.Engine, port string) error {
+	err := ginServer.Run(":" + port)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 注册路由
+func RegisterRouter(ginServer *gin.Engine, router ...router.RouterTemplate) error {
+	for _, r := range router {
+		err := r.PrepareRouter()
+		if err != nil {
+			return err
+		}
+		err = r.InitRouter(ginServer)
+	}
+	return nil
+}
