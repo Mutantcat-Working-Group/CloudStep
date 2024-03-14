9bcb34b25e79603609300d1912d1d24c53ac3a1f
洮羱芝闇 <shun_@outlook.com>
2024-03-14 11:08:52 +0800

    前端引入layui(汇报红但是仍然运行正常)

--- web/cloud-step-web-1g/src/App.vue
+++ web/cloud-step-web-1g/src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
+
+const open = function () {
+  layui.use(function () {
+    var layer = layui.layer;
+    layer.open({
+      type: 1, // page 层类型
+      area: ['500px', '300px'],
+      title: 'Hello layer',
+      shade: 0.6, // 遮罩透明度
+      shadeClose: true, // 点击遮罩区域，关闭弹层
+      maxmin: true, // 允许全屏最小化
+      anim: 0, // 0-6 的动画形式，-1 不开启
+      content: '<div style="padding: 32px;">一个普通的页面层，传入了自定义的 HTML</div>'
+    });
+  }
+  )
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <button type="button" class="layui-btn layui-bg-blue" @click="open()">蓝色按钮</button>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
@@ -21,9 +32,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- web/cloud-step-web-1g/src/main.ts
+++ web/cloud-step-web-1g/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import "layui/dist/css/layui.css"
 
 createApp(App).mount('#app')
