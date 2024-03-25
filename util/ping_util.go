e9939d6110361cec1b3f2046307366ca46a8caba
洮羱芝闇 <shun_@outlook.com>
2024-03-25 12:46:49 +0800

    更新ping预处理机制

--- util/ping_util.go
+++ util/ping_util.go
@@ -2,10 +2,13 @@ package util
 
 import (
 	"net"
+	"strings"
 	"time"
 )
 
 func GetTCPSpeed(url string, resultChan chan string) {
+	// 先把地址形式转换为ip:port
+	url = urlToIPP(url)
 	timeout := time.Duration(500 * time.Millisecond)
 	start := time.Now()
 	conn, err := net.DialTimeout("tcp", url, timeout)
@@ -20,7 +23,17 @@ func GetTCPSpeed(url string, resultChan chan string) {
 
 // 将任何链接转换为IP+端口的形式
 func urlToIPP(rawURL string) string {
+	// 去过前面有没有http://或者https://
+	if rawURL[:7] == "http://" {
+		rawURL = rawURL[7:]
+	}
+	if rawURL[:8] == "https://" {
+		rawURL = rawURL[8:]
+	}
+	// 如果后面有任何路径，去掉路径
+	if strings.Contains(rawURL, "/") {
+		rawURL = rawURL[:strings.Index(rawURL, "/")]
+	}
 
-	return ""
-
+	return rawURL
 }
